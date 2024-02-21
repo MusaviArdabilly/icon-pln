3378098f89fece23c39419213e83f19b93f1ff56
Musavi Ardabilly Taufik <201musavi@gmail.com>
2024-02-21 15:00:59 +0700

    fix: layout sidebar admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -151,14 +151,6 @@
 							<!-- Divider -->
 							<hr class="sidebar-divider d-none d-md-block">
 
-							<!-- Heading -->
-							<div class="sidebar-heading">
-									User
-							</div>
-
-							<!-- Divider -->
-							<hr class="sidebar-divider d-none d-md-block">
-
 						@else
 
 							<!-- Nav Item -->
