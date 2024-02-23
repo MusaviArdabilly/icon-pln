10ecc15f46343e8228364432fb87cb20cfd01152
Musavi Ardabilly Taufik <201musavi@gmail.com>
2024-02-23 14:37:02 +0700

    update: make status position (flow) as dynamic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(UserSeeder::class);
+        $this->call(FlowPositionSeeder::class);
         $this->call(IdeaSeeder::class);
         $this->call(InnovationSeeder::class);
         $this->call(CommentSeeder::class);
--- database/seeders/FlowPositionSeeder.php
+++ database/seeders/FlowPositionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\FlowPosition;
 
 class FlowPositionSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class FlowPositionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        FlowPosition::create([
+            'name' => 'Problem'
+        ]);
+        FlowPosition::create([
+            'name' => 'Design'
+        ]);
+        FlowPosition::create([
+            'name' => 'Prototype'
+        ]);
+        FlowPosition::create([
+            'name' => 'Evaluation'
+        ]);
+        FlowPosition::create([
+            'name' => 'Report'
+        ]);
     }
 }
